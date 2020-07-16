1b58fc0ec06d1c848e707703359b8f0c18cf702d
arainko <aleksander.rainko99@gmail.com>
2020-07-16 10:19:22 +0200

    Initial commit, dependency configuration

---
+++ app/src/main/java/com/rainko/albloom/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rainko.albloom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.HiltAndroidApp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
